b3b1f19de2b980cb3e12ae4112a1ec2c9380ffd7
Felipe <verificacoesfelipe@gmailcom>
2024-05-07 10:08:54 -0300

    changed some code regarding settings

--- SaveFile.cs
+++ SaveFile.cs
@@ -1,21 +1,20 @@
 using System.Text.Json;
 using CookiesCookbook;
 
+
 public class SaveFile
 {
-    private const string FileName = "recipes";
-
     public static void Save(Recipe recipe)
     {
         switch (Settings.SaveOption)
         {
             case "Txt":
                 string recipeJson = JsonSerializer.Serialize(recipe);
-                File.AppendAllText(FileName + ".json", $"\n{recipeJson}");
+                File.AppendAllText(Settings.FileName + ".json", $"\n{recipeJson}");
                 break;
             case "Json":
                 string recipeTxt = recipe.ToString();
-                File.AppendAllText(FileName + ".txt", $"\n{recipeTxt}");
+                File.AppendAllText(Settings.FileName + ".txt", $"\n{recipeTxt}");
                 break;
         }
     }
